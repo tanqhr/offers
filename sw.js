be0442b9f231ca0ea9a60cc82c6420a256203a52
Tanya Hristova <122736535+tanqhr@users.noreply.github.com>
2026-05-28 20:14:46 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,32 +1,32 @@
-// const CACHE_NAME = "offers-cache-v1";
+const CACHE_NAME = "offers-cache-v1";
 
-// const urlsToCache = [
-//   "./",
-//   "./index.html",
-//   "./prices.json"
-// ];
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./prices.json"
+];
 
-// self.addEventListener("install", event => {
-//   event.waitUntil(
-//     caches.open(CACHE_NAME)
-//     .then(cache => cache.addAll(urlsToCache))
-//   );
-// });
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+    .then(cache => cache.addAll(urlsToCache))
+  );
+});
 
-// self.addEventListener("fetch", event => {
-//   event.respondWith(
-//     caches.match(event.request)
-//     .then(response => response || fetch(event.request))
-//   );
-// });
-// const CACHE_NAME = "crm-cache-v2";
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+    .then(response => response || fetch(event.request))
+  );
+});
+const CACHE_NAME = "crm-cache-v2";
 
-// const urlsToCache = [
-//     "./",
-//     "./index.html",
-//     "./prices.json",
-//     "./manifest.json"
-// ];
+const urlsToCache = [
+    "./",
+    "./index.html",
+    "./prices.json",
+    "./manifest.json"
+];
 
 
 // // INSTALL
@@ -87,20 +87,20 @@
 // });
 
 
-self.addEventListener("install", e => {
-    e.waitUntil(
-        caches.open("crm-cache").then(cache => {
-            return cache.addAll([
-                "./",
-                "./index.html",
-                "./prices.json"
-            ]);
-        })
-    );
-});
+// self.addEventListener("install", e => {
+//     e.waitUntil(
+//         caches.open("crm-cache").then(cache => {
+//             return cache.addAll([
+//                 "./",
+//                 "./index.html",
+//                 "./prices.json"
+//             ]);
+//         })
+//     );
+// });
 
-self.addEventListener("fetch", e => {
-    e.respondWith(
-        caches.match(e.request).then(r => r || fetch(e.request))
-    );
-});
+// self.addEventListener("fetch", e => {
+//     e.respondWith(
+//         caches.match(e.request).then(r => r || fetch(e.request))
+//     );
+// });
