fedd37f7cb255b690f8c4315a8246fdf5d04d9ab
Tanya Hristova <122736535+tanqhr@users.noreply.github.com>
2026-05-28 20:17:48 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,24 @@
-const CACHE_NAME = "offers-cache-v3";
+// const CACHE_NAME = "offers-cache-v3";
 
-const urlsToCache = [
-  "./",
-  "./index.html",
-  "./prices.json"
-];
+// const urlsToCache = [
+//   "./",
+//   "./index.html",
+//   "./prices.json"
+// ];
 
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-    .then(cache => cache.addAll(urlsToCache))
-  );
-});
+// self.addEventListener("install", event => {
+//   event.waitUntil(
+//     caches.open(CACHE_NAME)
+//     .then(cache => cache.addAll(urlsToCache))
+//   );
+// });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request)
-    .then(response => response || fetch(event.request))
-  );
-});
+// self.addEventListener("fetch", event => {
+//   event.respondWith(
+//     caches.match(event.request)
+//     .then(response => response || fetch(event.request))
+//   );
+// });
 // const CACHE_NAME = "crm-cache-v2";
 
 // const urlsToCache = [
@@ -87,20 +87,20 @@ self.addEventListener("fetch", event => {
 // });
 
 
-// self.addEventListener("install", e => {
-//     e.waitUntil(
-//         caches.open("crm-cache").then(cache => {
-//             return cache.addAll([
-//                 "./",
-//                 "./index.html",
-//                 "./prices.json"
-//             ]);
-//         })
-//     );
-// });
+self.addEventListener("install", e => {
+    e.waitUntil(
+        caches.open("crm-cache").then(cache => {
+            return cache.addAll([
+                "./",
+                "./index.html",
+                "./prices.json"
+            ]);
+        })
+    );
+});
 
-// self.addEventListener("fetch", e => {
-//     e.respondWith(
-//         caches.match(e.request).then(r => r || fetch(e.request))
-//     );
-// });
+self.addEventListener("fetch", e => {
+    e.respondWith(
+        caches.match(e.request).then(r => r || fetch(e.request))
+    );
+});
