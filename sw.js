687031c71ac5163b0caabff01ced4e03d9272328
Tanya Hristova <122736535+tanqhr@users.noreply.github.com>
2026-05-29 13:49:15 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,91 +1,3 @@
-// const CACHE_NAME = "offers-cache-v3";
-
-// const urlsToCache = [
-//   "./",
-//   "./index.html",
-//   "./prices.json"
-// ];
-
-// self.addEventListener("install", event => {
-//   event.waitUntil(
-//     caches.open(CACHE_NAME)
-//     .then(cache => cache.addAll(urlsToCache))
-//   );
-// });
-
-// self.addEventListener("fetch", event => {
-//   event.respondWith(
-//     caches.match(event.request)
-//     .then(response => response || fetch(event.request))
-//   );
-// });
-// const CACHE_NAME = "crm-cache-v2";
-
-// const urlsToCache = [
-//     "./",
-//     "./index.html",
-//     "./prices.json",
-//     "./manifest.json"
-// ];
-
-
-// // INSTALL
-// self.addEventListener("install", event => {
-
-//     event.waitUntil(
-
-//         caches.open(CACHE_NAME)
-//         .then(cache => cache.addAll(urlsToCache))
-
-//     );
-
-// });
-
-
-// // ACTIVATE
-// self.addEventListener("activate", event => {
-
-//     event.waitUntil(
-
-//         caches.keys().then(cacheNames => {
-
-//             return Promise.all(
-
-//                 cacheNames.map(cache => {
-
-//                     if (cache !== CACHE_NAME) {
-
-//                         return caches.delete(cache);
-
-//                     }
-
-//                 })
-
-//             );
-
-//         })
-
-//     );
-
-// });
-
-
-// // FETCH
-// self.addEventListener("fetch", event => {
-
-//     event.respondWith(
-
-//         caches.match(event.request)
-//         .then(response => {
-
-//             return response || fetch(event.request);
-
-//         })
-
-//     );
-
-// });
-
 
 self.addEventListener("install", e => {
     e.waitUntil(
@@ -93,8 +5,8 @@ self.addEventListener("install", e => {
             return cache.addAll([
                 "./",
                 "./index.html",
-                "./prices.json",
-                '/style.css?v=3'
+                "./prices.json"
+               
             ]);
         })
     );
